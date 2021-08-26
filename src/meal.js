1d9bb6a5426a5f360034fb48feb56e425a97cc7a
Joan Rasmussen <raz.joan@gmail.com>
2021-08-25 21:06:57 -0600

    Add meal.js and meal-test.js

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,47 @@
-function nameMenuItem() {
-
+function nameMenuItem(item) {
+  return "Delicious " + item;
 }
 
+function createMenuItem(item, price, type) {
+  var menuItem = {
+    name: item,
+    price: price,
+    type: type
+  }
+  return menuItem;
+}
+
+function addIngredients(ingredient, ingredients) {
+  if (ingredients.includes(ingredient)) {
+    // do nothing
+  } else {
+    ingredients.push(ingredient);
+  }
+  return ingredients;
+}
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`;
+}
+
+function decreasePrice(price) {
+  return price - (price * 0.1);
+}
+
+function createRecipe(title, ingredients, menuType) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuType
+  }
+  return recipe;
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
