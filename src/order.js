70bf9c4caf50e52ac7167be97d343a156fa7cb5b
Joan Rasmussen <raz.joan@gmail.com>
2021-08-26 18:24:37 -0600

    Add order.js and order-test.js

--- src/order.js
+++ src/order.js
@@ -1,7 +1,43 @@
+function takeOrder(order, deliveryOrders) {
+  if (deliveryOrders.length < 3) {
+    return deliveryOrders.push(order);
+  } else {
+    return deliveryOrders;
+  }
+}
+
+function refundOrder(orderNum, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].orderNumber === orderNum) {
+      deliveryOrders.splice(i, 1);
+    }
+  }
+  return deliveryOrders;
+}
+
+function listItems(deliveryOrders) {
+  var listOfItems = "";
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    listOfItems = listOfItems + deliveryOrders[i].item;
+    if (i < deliveryOrders.length - 1) {
+      listOfItems = listOfItems + ", ";
+    }
+  }
+  return listOfItems;
+}
+
+function searchOrder(deliveryOrders, itemName) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].item === itemName) {
+      return true;
+    }
+  }
+  return false;
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
